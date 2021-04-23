534f5e1455808a08b1b930efefbc3e224a48a923
Xiangmin Mo <xiangminmo6121@gmail.com>
2021-04-22 21:57:01 -0400

    added authentication from auth branch

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const { User } = db;
+const passport = require('../middlewares/authentication');
 
 //    POST   /
 //      Body: {
@@ -23,7 +24,7 @@ const { User } = db;
 //      }
 
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   res.json({
     page: 'api/user/',
   });
@@ -32,24 +33,35 @@ router.get('/', (req,res) => {
 
 // Post a new entry in the User table
 // Body needed: Username, password, email, and favorite stops (default empty) 
-router.post('/', (req, res) => {
+router.post('/signup', (req, res) => {
   let { username, password, email } = req.body;
-  
+
   User.create({ username, password, email })
     .then(user => {
-      res.status(201).json(user);
+      req.login(user, () => res.status(201).json(user));
     })
     .catch(err => {
-      res.status(400).json(err);
+      res.status(400).json({ msg: 'Failed Registration', err });
     });
 });
+
+router.post('/login',
+  passport.authenticate('local'),
+  (req, res) => {
+    res.json(req.user);
+  });
+
+router.post('/logout', (req, res) => {
+  req.logout();
+  res.status(200).json({ msg: 'Logout Successful' });
+})
 
 // Get favorite array given a username
 router.get('/favorite/:username', (req, res) => {
   const { username } = req.params;
   User.findByPk(username)
     .then(user => {
-      if(!user) {
+      if (!user) {
         return res.sendStatus(404);
       }
       res.status(200).json(user['favorites']);
@@ -61,12 +73,12 @@ router.put('/favorite/:username', (req, res) => {
   const { username } = req.params;
   User.findByPk(username)
     .then(user => {
-      if(!user) {
+      if (!user) {
         return res.sendStatus(404);
       }
 
       const stopId = req.body.stopId
-      if(stopId && !user.favorites.includes(stopId)) {
+      if (stopId && !user.favorites.includes(stopId)) {
         var tmp = user.favorites.slice()
         tmp.push(stopId)
         user.favorites = tmp
@@ -87,12 +99,12 @@ router.delete('/favorite/:username', (req, res) => {
   const { username } = req.params;
   User.findByPk(username)
     .then(user => {
-      if(!user) {
+      if (!user) {
         return res.sendStatus(404);
       }
 
       const stopId = req.body.stopId
-      if(stopId && user.favorites.includes(stopId)) {
+      if (stopId && user.favorites.includes(stopId)) {
         const tmp = user.favorites.filter((val, i) => {
           return val !== stopId
         })
